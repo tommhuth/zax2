a201b62783a94a1798f022528630f2c336f6e70c
Tomm <tomm.huth@hotmail.com>
2024-01-06 16:51:38 +0100

    less frequent

--- src/data/world/validator.ts
+++ src/data/world/validator.ts
@@ -2,7 +2,7 @@ import { store } from "../store"
 import { WorldPart, WorldPartType } from "../types"
 
 let lastBossAt = new Date()
-let bossInterval = 60_000 * 1
+let bossInterval = 60_000 * 2
 
 export const validator: Record<WorldPartType, (previous: WorldPart) => boolean> = {
     [WorldPartType.DEFAULT]: () => true,
