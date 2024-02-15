2aa62808a2d4e9f7fc61ada8f51e38129972b3f8
Tomm Huth <tomm.huth@hotmail.com>
2024-02-15 23:50:43 +0100

    shadows

--- src/components/world/models/instances/Barrels.tsx
+++ src/components/world/models/instances/Barrels.tsx
@@ -6,61 +6,92 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
 import { useLoader } from "@react-three/fiber"
 
 export default function Barrels() {
-    let [
-        barrel1, barrel2, barrel3, barrel4, 
-    ] = useLoader(GLTFLoader, [
+    let [barrel1, barrel2, barrel3, barrel4] = useLoader(GLTFLoader, [
         "/models/barrel1.glb",
         "/models/barrel2.glb",
         "/models/barrel3.glb",
-        "/models/barrel4.glb", 
+        "/models/barrel4.glb",
     ])
 
     return (
         <>
-            <InstancedMesh name="barrel1" count={15}>
-                <primitive object={(barrel1.nodes.barrel as Mesh).geometry} dispose={null} attach="geometry" />
+            <InstancedMesh
+                name="barrel1"
+                count={15}
+                castShadow
+                receiveShadow
+            >
+                <primitive
+                    object={(barrel1.nodes.barrel as Mesh).geometry}
+                    dispose={null}
+                    attach="geometry"
+                />
                 <MeshRetroMaterial
-                    fogDensity={.35}
+                    fogDensity={0.35}
                     name="barrel1"
-                    fogHeight={.6}
+                    fogHeight={0.6}
                     color={barellcolor}
                     emissive={barellcolor}
                     emissiveIntensity={barrellEmissiveIntensity}
                 />
             </InstancedMesh>
 
-            <InstancedMesh name="barrel2" count={15}>
-                <primitive object={(barrel2.nodes.barrel2 as Mesh).geometry} attach="geometry" />
+            <InstancedMesh
+                castShadow
+                receiveShadow
+                name="barrel2"
+                count={15}
+            >
+                <primitive
+                    object={(barrel2.nodes.barrel2 as Mesh).geometry}
+                    attach="geometry"
+                />
                 <MeshRetroMaterial
-                    fogDensity={.35}
+                    fogDensity={0.35}
                     name="barrel2"
                     color={barellcolor}
-                    fogHeight={.6}
+                    fogHeight={0.6}
                     emissive={barellcolor}
                     emissiveIntensity={barrellEmissiveIntensity}
                 />
             </InstancedMesh>
 
-            <InstancedMesh name="barrel3" count={15}>
-                <primitive object={(barrel3.nodes.barrel3 as Mesh).geometry} attach="geometry" />
+            <InstancedMesh
+                castShadow
+                receiveShadow
+                name="barrel3"
+                count={15}
+            >
+                <primitive
+                    object={(barrel3.nodes.barrel3 as Mesh).geometry}
+                    attach="geometry"
+                />
                 <MeshRetroMaterial
-                    fogDensity={.35}
+                    fogDensity={0.35}
                     name="barrel3"
                     color={barellcolor}
-                    fogHeight={.6}
+                    fogHeight={0.6}
                     emissive={barellcolor}
                     emissiveIntensity={barrellEmissiveIntensity}
                 />
             </InstancedMesh>
 
-            <InstancedMesh name="barrel4" count={15}>
-                <primitive object={(barrel4.nodes.barrel4 as Mesh).geometry} attach="geometry" />
+            <InstancedMesh
+                castShadow
+                receiveShadow
+                name="barrel4"
+                count={15}
+            >
+                <primitive
+                    object={(barrel4.nodes.barrel4 as Mesh).geometry}
+                    attach="geometry"
+                />
                 <MeshRetroMaterial
-                    fogDensity={.35}
+                    fogDensity={0.35}
                     name="barrel4"
                     color={barellcolor}
                     emissive={barellcolor}
-                    fogHeight={.6}
+                    fogHeight={0.6}
                     emissiveIntensity={barrellEmissiveIntensity}
                 />
             </InstancedMesh>
