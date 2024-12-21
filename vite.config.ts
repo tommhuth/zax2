3771b4391f91a0f46f8e2b9d04209972583deade
Tomm Huth <tomm.huth@hotmail.com>
2024-12-21 09:07:48 +0100

    no inlining

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
     server: {
         port: 3000,
     },
+    build: {
+        assetsInlineLimit: 0,
+    },
     assetsInclude: ["**/*.glb"],
     resolve: {
         alias: {
